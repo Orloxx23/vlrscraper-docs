0b340d079bedae1cea320165d29070f24d4060f1
Orlando Mina <65779635+Orloxx23@users.noreply.github.com>
2023-06-27 18:04:57 -0500

    get player playground

---
+++ pages/api/player.js
@@ -0,0 +1,21 @@
+export default async function handler(req, res) {
+  const { id } = JSON.parse(req.body);
+
+  const url = `https://valorant-esports1.p.rapidapi.com/v1/players/${id}`;
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
+      "X-RapidAPI-Host": process.env.RAPIDAPI_HOST,
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    res.status(200).json({ result });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+}
