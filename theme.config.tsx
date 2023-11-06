bb233a3cf38da7dd9ee4157c9c43c25af0776945
Orloxx23 <madorlix@gmail.com>
2023-11-06 00:43:55 -0500

    Add logo and update footer in theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,34 +1,39 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>Valorant Esports API v1.0.3</span>,
+  logo: (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <img src="https://i.imgur.com/ZSMbOPL.png" width={64} height={64} />
+      <span>Valorant Esports API v1.0.3</span>
+    </div>
+  ),
   project: {
-    link: 'https://github.com/Orloxx23/vlresports',
+    link: "https://github.com/Orloxx23/vlresports",
   },
   /*chat: {
     link: 'https://discord.com',
   },*/
-  docsRepositoryBase: 'https://github.com/Orloxx23/vlresports',
+  docsRepositoryBase: "https://github.com/Orloxx23/vlresports",
   footer: {
     text: (
       <span>
-        Apache 2.0 {new Date().getFullYear()} ©{' '}
+        Apache 2.0 {new Date().getFullYear()} ©{" "}
         <a href="https://orlandomm.net" target="_blank">
           Orloxx
         </a>
         .
       </span>
-    )
+    ),
   },
   editLink: {
-    text: 'Edit this page on GitHub →',
+    text: "Edit this page on GitHub →",
   },
   useNextSeoProps() {
-      return {
-        titleTemplate: '%s | Valorant Esports API',
-      }
+    return {
+      titleTemplate: "%s | Valorant Esports API",
+    };
   },
-}
+};
 
-export default config
+export default config;
